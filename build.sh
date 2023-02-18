89a67267be4748ddcb2911e1daecfdae77707f61
q962_ <1250079251@qq.com>
2023-02-18 11:12:04 +0800

    update 2.39.2, fix build command, fix usage of linuxdeploy

--- build.sh
+++ build.sh
@@ -6,8 +6,8 @@ if [ $? -ne 0 ] ; then
     docker build --tag git-appimage .
 fi
 
-GIT_VERSION=${1:-2.36.1}
+GIT_VERSION=${1:-2.39.2}
 
-docker run --name gitcontainer -e GIT_VERSION="$GIT_VERSION" git-appimage $GIT_VERSION
+docker run --name gitcontainer -e GIT_VERSION="$GIT_VERSION" git-appimage
 docker cp gitcontainer:/opt/git.$GIT_VERSION.appimage .
 docker rm -f gitcontainer
--- buildgit.sh
+++ buildgit.sh
@@ -2,7 +2,10 @@
 
 cd /app
 
-GIT_VERSION=${1:-${GIT_VERSION:-2.36.1}}
+if [ -z "GIT_VERSION" ] ; then
+    echo "Requires github git tag number"
+    exit -1;
+fi
 
 if [ ! -s git.$GIT_VERSION.tar.gz ] ; then
     wget "https://github.com/git/git/archive/v$GIT_VERSION.tar.gz" -O git.$GIT_VERSION.tar.gz
@@ -17,15 +20,12 @@ fi
 
 cd git-$GIT_VERSION
 
-# build requires c99
-gnu99_number=`awk "/gnu99\" at the end/{print NR+1}" Makefile`
-sed "$gnu99_number{s/$/ -std=gnu99/}" -i Makefile
-
-make prefix=/usr all
-make prefix=/usr DESTDIR=/AppDir install
-
-find /AppDir -type f -perm /a+x -exec strip {} \;
+make CFLAGS="-O2 -Wall -std=gnu99" prefix=/usr DESTDIR=/AppDir install || exit -1
 
 ## Build the appimage
 chmod u+x /AppDir/AppRun
-OUTPUT="/opt/git.${GIT_VERSION}.appimage" /usr/bin/linuxdeploy --appdir=/AppDir --output=appimage
+
+OUTPUT="/opt/git.${GIT_VERSION}.appimage" \
+    /usr/bin/linuxdeploy \
+    --appdir=/AppDir --output=appimage \
+    --deploy-deps-only=/AppDir/usr/libexec/git-core/
